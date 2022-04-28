7da4f4af5021ea7da4577c93f82e0088bd19a987
RedBlueDevil <noahtang1997@yahoo.com>
2022-04-28 17:23:41 -0500

    start of 4 in main

--- encrypt-driver.c
+++ encrypt-driver.c
@@ -11,8 +11,42 @@ void reset_finished()
 	
 }
 
-int main(int argc, char *argv[]) {
-	init("in.txt", "out.txt", "log.txt"); 
+int main(int argc, char *argv[]) 
+{
+    char c;
+    FILE *finput, *foutput, *flog;
+    if (argc == 3)
+    {
+        finput = fopen(argv[1], "w");
+        foutput = fopen(argv[2], "w");
+        flog = fopen(argv[3], "w");
+    }
+    else
+    {
+        printf("please include input file name, output name, and log filename");
+        exit();
+    }
+
+	init(finput, foutput, flog); 
+
+    printf("please give input buffer size.");
+    int ibuffer;
+    scanf("%d", &ibuffer);
+    if (ibuffer <= 1)
+    {
+        printf("input buffer needs to be greater than 1");
+        exit();
+    }
+
+    printf("please give output buffer size.");
+    int obuffer;
+    scanf("%d", &obuffer);
+    if (obuffer <= 1)
+    {
+        printf("output buffer needs to be greater than 1");
+        exit();
+    }
+
 	char c;
 	while ((c = read_input()) != EOF) { 
 		count_input(c); 

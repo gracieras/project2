95dad4dd01ebbee2a19b7a0eee9598cf0b34831e
RedBlueDevil <noahtang1997@yahoo.com>
2022-04-28 18:51:26 -0500

    more circular buffer stuff

--- encrypt-driver.c
+++ encrypt-driver.c
@@ -56,6 +56,11 @@ int main(int argc, char *argv[])
     // int obufferlength = 0;
     // int obufferreadIndex = 0;
     // int obufferwriteIndex = 0;
+    uint8_t * ibuffer  = malloc(ibuffersize * sizeof(uint8_t));
+    cbuf_handle_t ime = circular_buf_init(ibuffer, ibuffersize);
+
+    uint8_t * obuffer  = malloc(obuffersize * sizeof(uint8_t));
+    cbuf_handle_t ome = circular_buf_init(obuffer, obuffersize);
 
 	char c;
 	while ((c = read_input()) != EOF) { 
@@ -107,3 +112,150 @@ size_t circular_buf_capacity(cbuf_handle_t me);
 
 /// Returns the current number of elements in the buffer
 size_t circular_buf_size(cbuf_handle_t me);
+
+// The hidden definition of our circular buffer structure
+struct circular_buf_t {
+	uint8_t * buffer;
+	size_t head;
+	size_t tail;
+	size_t max; //of the buffer
+	bool full;
+};
+
+// // User provides struct
+// void circular_buf_init(circular_buf_t* me, uint8_t* buffer, size_t size);
+
+// // Return a concrete struct
+// circular_buf_t circular_buf_init(uint8_t* buffer, size_t size);
+
+// // Return a pointer to a struct instance - preferred approach
+// cbuf_handle_t circular_buf_init(uint8_t* buffer, size_t size);
+
+cbuf_handle_t circular_buf_init(uint8_t* buffer, size_t size)
+{
+	assert(buffer && size);
+
+	cbuf_handle_t cbuf = malloc(sizeof(circular_buf_t));
+	assert(cbuf);
+
+	cbuf->buffer = buffer;
+	cbuf->max = size;
+	circular_buf_reset(cbuf);
+
+	assert(circular_buf_empty(cbuf));
+
+	return cbuf;
+}
+
+void circular_buf_reset(cbuf_handle_t me)
+{
+    assert(me);
+
+    me->head = 0;
+    me->tail = 0;
+    me->full = false;
+}
+
+void circular_buf_free(cbuf_handle_t me)
+{
+	assert(me);
+	free(me);
+}
+
+bool circular_buf_full(cbuf_handle_t me)
+{
+	assert(me);
+
+	return me->full;
+}
+
+bool circular_buf_empty(cbuf_handle_t me)
+{
+	assert(me);
+
+	return (!me->full && (me->head == me->tail));
+}
+
+size_t circular_buf_capacity(cbuf_handle_t me)
+{
+	assert(me);
+
+	return me->max;
+}
+
+size_t circular_buf_size(cbuf_handle_t me)
+{
+	assert(me);
+
+	size_t size = me->max;
+
+	if(!me->full)
+	{
+		if(me->head >= me->tail)
+		{
+			size = (me->head - me->tail);
+		}
+		else
+		{
+			size = (me->max + me->head - me->tail);
+		}
+	}
+
+	return size;
+}
+
+static void advance_pointer(cbuf_handle_t me)
+{
+	assert(me);
+
+	if(me->full)
+   	{
+		if (++(me->tail) == me->max) 
+		{ 
+			me->tail = 0;
+		}
+	}
+
+	if (++(me->head) == me->max) 
+	{ 
+		me->head = 0;
+	}
+	me->full = (me->head == me->tail);
+}
+
+static void retreat_pointer(cbuf_handle_t me)
+{
+	assert(me);
+
+	me->full = false;
+	if (++(me->tail) == me->max) 
+	{ 
+		me->tail = 0;
+	}
+}
+
+void circular_buf_put(cbuf_handle_t me, uint8_t data)
+{
+	assert(me && me->buffer);
+
+    me->buffer[me->head] = data;
+
+    advance_pointer(me);
+}
+
+int circular_buf_get(cbuf_handle_t me, uint8_t * data)
+{
+    assert(me && data && me->buffer);
+
+    int r = -1;
+
+    if(!circular_buf_empty(me))
+    {
+        *data = me->buffer[me->tail];
+        retreat_pointer(me);
+
+        r = 0;
+    }
+
+    return r;
+}

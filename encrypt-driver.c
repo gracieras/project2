aad6a139872dce0988cfd38dc9236d4f1a139c20
ger <ger@iastate.edu>
2022-04-29 00:48:16 -0500

    create threads and begin working on functionality of them

--- encrypt-driver.c
+++ encrypt-driver.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <pthread.h>
 #include "encrypt-module.h"
+
+int *inbuffer;     //int array buffer to hold input
+int *outbuffer;    //int array buffer to hold output
+
+//counters for read/write and counters for buffers
+int reader, incounter, encryptincounter, encryptoutcounter, outcounter, writer;
+
+int in,out; //size of in/out buffers
 
 void reset_requested() 
 {
@@ -11,6 +20,128 @@ void reset_requested()
 void reset_finished() 
 {
 	
+}
+
+//thread method to read each character in the input file as they buffer is ready to receive them,
+//calls read_input from encrypt-module.h to iterate thorugh file and places each character in the inbuffer.
+//signals that characters are ready to be counted
+void *readFile(void *param) {
+
+    int c;
+    reader = 0;
+    while ((c = read_input()) != EOF) {
+
+        while(resetting == 1) {
+
+        }
+        sem_wait(&readsem);        
+        inbuffer[reader] = c;
+        sem_post(&countinsem);
+        reader = (reader + 1) % in;
+    }   
+    inbuffer[reader] = EOF;
+    sem_post(&countinsem);    
+    pthread_exit(0);
+}
+
+//thread method to count each character in the inbuffer and add to total count 
+//and character counts. after character is counted it signals it is ready to be encrypted
+void *countInBuffer(void *param) {
+
+    incounter = 0;
+    while (1) {
+
+        while(resetting == 1) {
+
+        }
+        sem_wait(&countinsem);
+
+        if(inbuffer[incounter] == EOF) {
+            sem_post(&encryptinsem);
+            break;
+        }
+        count_input(inbuffer[incounter]);
+        sem_post(&encryptinsem);
+        incounter = (incounter + 1) % in;
+    }
+    pthread_exit(0);
+}
+
+//thread method that encrypts characters as they become available in the inbuffer
+//writes encrypted character to outbuffer and signals that character is ready to be counter
+//signals to reader that encrypted character can be overwritten through readFile()
+//signals that encrypted character is ready to be counted
+void *encrypt(void *param) {
+
+    encryptincounter = 0;
+    encryptoutcounter = 0;
+    while(1) {
+        
+        while(resetting == 1) {
+
+        }
+        sem_wait(&encryptinsem);
+
+        if(inbuffer[encryptincounter] == EOF) {
+            outbuffer[encryptoutcounter] = EOF;
+            sem_post(&countoutsem); //potential error
+            break;
+        }
+        sem_wait(&encryptoutsem);
+        outbuffer[encryptoutcounter] = caesar_encrypt(inbuffer[encryptincounter]);
+        sem_post(&readsem);
+        sem_post(&countoutsem);
+
+        encryptincounter = (encryptincounter + 1) % in;
+        encryptoutcounter = (encryptoutcounter + 1) % out;
+    }
+    pthread_exit(0);
+}
+
+//method thread to count total and count each character in the outbuffer
+//once counted, signals that the character is ready to be written to output file
+void *countOutBuffer(void *param) {
+
+    outcounter = 0;
+    while(1) {
+
+        while(resetting == 1) {
+
+        }
+        sem_wait(&countoutsem);
+        
+        if(outbuffer[outcounter] == EOF) {
+            sem_post(&writesem);
+            break;
+        }   
+        count_output(outbuffer[outcounter]);
+        sem_post(&writesem);
+        outcounter = (outcounter + 1) % out;
+    }
+    pthread_exit(0);
+}
+
+//method thread to write character to output file
+//once character is written, signals encrypt thread that the output buffer is ready to 
+//receive new characters
+void *writeFile(void *param) {
+
+    writer = 0;
+    while(1) {
+
+        while(resetting == 1) {
+
+        }
+        sem_wait(&writesem);
+		
+        if(outbuffer[writer] == EOF) {
+            break;
+        }
+        write_output(outbuffer[writer]);
+        sem_post(&encryptoutsem);
+        writer = (writer + 1) % out;
+    }
+    pthread_exit(0);
 }
 
 int main(int argc, char *argv[]) 
@@ -71,6 +202,28 @@ int main(int argc, char *argv[])
 	} 
 	printf("End of file reached.\n"); 
 	log_counts();
+
+	pthread_t reader;
+	pthread_t inputCounter;
+	pthread_t encryptor;
+	pthread_t outputCounter;
+	pthread_t writer;
+
+	pthread_create(&reader, &attr, &readFile, 0);
+    pthread_create(&inputCounter, &attr, &countInBuffer, 0);
+    pthread_create(&encryptor, &attr, &encrypt, 0);
+    pthread_create(&outputCounter, &attr, &countOutBuffer, 0);
+    pthread_create(&writer, &attr, &writeFile, 0);
+
+	pthread_join(reader, NULL);
+    pthread_join(inputCounter, NULL);
+    pthread_join(encryptor, NULL);
+    pthread_join(outputCounter, NULL);
+    pthread_join(writer, NULL);
+
+	free(inbuffer);
+    free(outbuffer);
+
 }
 
 typedef struct circular_buf_t circular_buf_t;

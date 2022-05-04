399ea1d1dca388d012784a9a644d36f64ab8c828
RedBlueDevil <noahtang1997@yahoo.com>
2022-05-04 14:16:16 -0500

    changed makefile to point to test-encrypt-driver.c

--- test-encrypt-driver.c
+++ test-encrypt-driver.c
@@ -13,10 +13,6 @@
 char *inbuffer;     //char array buffer to hold input
 char *outbuffer;    //char array buffer to hold output
 
-// sem_t encryptinsem;
-// sem_t encryptoutsem;
-// sem_t countinsem;
-// sem_t countoutsem;
 sem_t readsem; //space_inside_inputBuffer
 sem_t writesem; //space_inside_outputBuffer
 sem_t inputLock;
@@ -28,6 +24,8 @@ int outputData; //stuff_inside_OutBuffer;
 int iCounter; //chars_to_count_input;
 int oCounter; //chars_to_count_output;
 int resetting;
+
+bool isDone;
 
 //counters for read/write and counters for buffers
 int reader, incounter, encryptincounter, encryptoutcounter, outcounter, writer;
@@ -55,10 +53,6 @@ void reset_requested()
     }
 	log_counts();
 
-
-    printf("reset requested test");
-    printf("\n");
-
     // printf("Total input count with the current key is %d\n", get_input_total_count());
     // printf("A:%d B:%d C:%d D:%d E:%d F:%d G:%d H:%d I:%d J:%d K:%d L:%d M:%d N:%d O:%d P:%d Q:%d R:%d S:%d T:%d U:%d V:%d W:%d X:%d Y:%d Z:%d\n", get_input_count('a'), get_input_count('b'), get_input_count('c'), get_input_count('d'), get_input_count('e'), get_input_count('f'), get_input_count('g'), get_input_count('h'), get_input_count('i'), get_input_count('j'), get_input_count('k'), get_input_count('l'), get_input_count('m'), get_input_count('n'), get_input_count('o'), get_input_count('p'), get_input_count('q'), get_input_count('r'), get_input_count('s'), get_input_count('t'), get_input_count('u'), get_input_count('v'), get_input_count('w'), get_input_count('x'), get_input_count('y'), get_input_count('z'));
     // printf("Total output count with the current key is %d\n", get_output_total_count());
@@ -74,7 +68,7 @@ void reset_finished()
 //thread method to read each character in the input file as they buffer is ready to receive them,
 //calls read_input from encrypt-module.h to iterate thorugh file and places each character in the inbuffer.
 //signals that characters are ready to be counted
-void *readFile(void *param) 
+void *readFile() 
 {
     char c;
     reader = 0;
@@ -88,16 +82,21 @@ void *readFile(void *param)
         sem_wait(&readsem);
         sem_wait(&inputLock);
 
-        inbuffer[reader] = c;
+        inbuffer[reader % in] = c;
 
-        reader = (reader + 1) % in;
+        // *(input_buffer + (currentIndex % input_buffer_size)) = c;
+
+        reader++;
         inputData++;
         iCounter++;
 
         sem_post(&inputLock);
-    }   
-    inbuffer[reader] = EOF;
-    // sem_post(&countinsem);    
+    }
+
+    // inbuffer[reader % in] = EOF;
+    // sem_post(&countinsem);  
+
+    isDone = true;  
     pthread_exit(0);
 }
 //thread method to count each character in the inbuffer and add to total count 
@@ -110,16 +109,15 @@ void *countInBuffer(void *param)
     {
         sem_wait(&inputLock);
         
-        if(inbuffer[incounter] == EOF) 
+        if(iCounter == 0 && isDone) 
         {
-            // sem_post(&encryptinsem);
             sem_post(&inputLock);
             break;
         }
         
-        count_input(inbuffer[incounter]);
+        count_input(inbuffer[incounter % in]);
 
-        incounter = (incounter + 1) % in;
+        incounter++;
         iCounter--;
 
         sem_post(&inputLock);
@@ -142,10 +140,9 @@ void *encryptFile(void *param)
         sem_wait(&inputLock);
         sem_wait(&outputLock);
 
-        if(inbuffer[encryptincounter] == EOF) 
+        if(iCounter == 0 && isDone) 
         {
-            outbuffer[encryptoutcounter] = EOF;
-            
+            // outbuffer[encryptoutcounter] = EOF;
             sem_post(&writesem);
             sem_post(&inputLock);
             sem_post(&outputLock);
@@ -153,14 +150,14 @@ void *encryptFile(void *param)
         }
 
         // sem_wait(&encryptoutsem);
-        outbuffer[encryptoutcounter] = encrypt(inbuffer[encryptincounter]);
+        outbuffer[encryptoutcounter % out] = encrypt(inbuffer[encryptincounter % in]);
 
         inputData--;
         outputData++;
         oCounter++;
 
-        encryptincounter = (encryptincounter + 1) % in;
-        encryptoutcounter = (encryptoutcounter + 1) % out;
+        encryptincounter++;
+        encryptoutcounter++;
         
         sem_post(&readsem);
         sem_post(&inputLock);
@@ -179,15 +176,15 @@ void *countOutBuffer(void *param)
     {
         sem_wait(&outputLock);
         
-        if(outbuffer[outcounter] == EOF) 
+        if(oCounter == 0 && isDone) 
         {
             sem_post(&outputLock);
             break;
         }
 
-        count_output(outbuffer[outcounter]);
+        count_output(outbuffer[outcounter % out]);
         
-        outcounter = (outcounter + 1) % out;
+        outcounter++;
         oCounter--;
 
         sem_post(&outputLock);
@@ -206,14 +203,15 @@ void *writeFile(void *param)
     {
         sem_wait(&outputLock);
 		
-        if(outbuffer[writer] == EOF) 
+        if(oCounter == 0 && isDone) 
         {
             sem_post(&outputLock);
             break;
         }
 
-        write_output(outbuffer[writer]);
-        writer = (writer + 1) % out;
+        write_output(outbuffer[writer % out]);
+
+        writer++;
         outputData--;
 
         sem_post(&writesem);
@@ -274,13 +272,10 @@ int main(int argc, char *argv[])
     outputData = 0;
     iCounter = 0;
     oCounter = 0;
+    isDone = false;
 
     
     //initialize semaphores
-    // sem_init(&encryptinsem, 0, 0);
-    // sem_init(&encryptoutsem, 0, 1);
-    // sem_init(&countinsem, 0, 0);
-    // sem_init(&countoutsem, 0, 0);
     sem_init(&readsem, 0, in);
     sem_init(&writesem, 0, out);
     sem_init(&inputLock, 0, 1);
@@ -293,14 +288,14 @@ int main(int argc, char *argv[])
 	pthread_t encryptor;
 	pthread_t outputCounter;
 	pthread_t writer;
-	pthread_attr_t attr;
+	// pthread_attr_t attr;
 
-    pthread_attr_init(&attr);
-	pthread_create(&reader, &attr, &readFile, NULL);
-    pthread_create(&inputCounter, &attr, &countInBuffer, NULL);
-    pthread_create(&encryptor, &attr, &encryptFile, NULL);
-    pthread_create(&outputCounter, &attr, &countOutBuffer, NULL);
-    pthread_create(&writer, &attr, &writeFile, NULL);
+    // pthread_attr_init(&attr);
+	pthread_create(&reader, NULL, &readFile, NULL);
+    pthread_create(&inputCounter, NULL, &countInBuffer, NULL);
+    pthread_create(&encryptor, NULL, &encryptFile, NULL);
+    pthread_create(&outputCounter, NULL, &countOutBuffer, NULL);
+    pthread_create(&writer, NULL, &writeFile, NULL);
 
 	pthread_join(reader, NULL);
     pthread_join(inputCounter, NULL);
@@ -309,24 +304,10 @@ int main(int argc, char *argv[])
     pthread_join(writer, NULL);
 
 	sem_destroy(&readsem);
-    // sem_destroy(&countinsem);
-    // sem_destroy(&encryptinsem);
-    // sem_destroy(&encryptoutsem);
-    // sem_destroy(&countoutsem);
     sem_destroy(&writesem);
     sem_destroy(&inputLock);
     sem_destroy(&outputLock);
     sem_destroy(&reset);
-
-    //log character counts
-    // char c;
-	// while ((c = read_input()) != EOF) 
-    // { 
-	// 	count_input(c); 
-	// 	c = encrypt(c); 
-	// 	count_output(c); 
-	// 	write_output(c); 
-	// } 
 	
 	log_counts();
     printf("End of file reached."); 
